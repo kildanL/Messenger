c1d882b8d5ffb23175c9efb806bf5d0ba43aa44b
kildanL <kildan325@gmail.com>
2023-08-18 14:42:01 +0300

    Add main component

--- client/src/components/Main.jsx
+++ client/src/components/Main.jsx
@@ -1,6 +1,62 @@
-import React from "react";
+import React, { useState } from "react";
 import st from "../styles/Main.module.css";
+import { Link } from "react-router-dom";
+
+const FIELDS = {
+    NAME: "name",
+    ROOM: "room",
+};
 
 export const Main = () => {
-    return <div>Main</div>;
+    const { NAME, ROOM } = FIELDS;
+
+    const [values, setValues] = useState({ [NAME]: "", [ROOM]: "" });
+
+    function handleChange({ target: { value, name } }) {
+        setValues({ ...value, [name]: value });
+    }
+
+    return (
+        <div className={st.wrap}>
+            <div className={st.container}>
+                <h1 className={st.heading}>Join</h1>
+
+                <form className={st.form}>
+                    <div className={st.group}>
+                        <input
+                            type="text"
+                            name="name"
+                            value={values[NAME]}
+                            placeholder="Введите никнейм"
+                            className={st.input}
+                            onChange={handleChange}
+                            autoComplete="off"
+                            required
+                        ></input>
+                    </div>
+                    <div className={st.group}>
+                        <input
+                            type="text"
+                            name="room"
+                            value={values[ROOM]}
+                            placeholder="Введите название комнаты"
+                            className={st.input}
+                            onChange={handleChange}
+                            autoComplete="off"
+                            required
+                        ></input>
+                    </div>
+
+                    <Link
+                        className={st.group}
+                        to={`/chat?name=${values[NAME]}&room=${values[ROOM]}`}
+                    >
+                        <button type="submit" className={st.button}>
+                            Войти
+                        </button>
+                    </Link>
+                </form>
+            </div>
+        </div>
+    );
 };
